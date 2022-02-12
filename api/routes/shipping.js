1ac3af7da55a60b7751105673518355f1f4126d5
The-government0 <eklofcas@arcada.fi>
2022-02-13 01:33:05 +0200

    query params fixed and minor changes

--- api/routes/shipping.js
+++ api/routes/shipping.js
@@ -37,62 +37,18 @@ router.post('/reduce', async (req,res) =>{
     id = req.query.id;
     quantity = req.query.quantity;
     email = req.query.email;
-
-    if(id == null || quantity == null) {
-        res.status(400).json({
-            status: res.statusCode,
-            message: 'Id or Quantity missing'
-        });
-    }
-    else if(!(quantity > 0)) {
-       
-        res.status(400).json({
-            status: res.statusCode,
-            message: 'Quantity must be a number over 0'
-        });
-    }
-    let storageStatus = await storageCaller(id, quantity);
-
-    //console.log(storageStatus);
-
-    if(storageStatus == 404){
-        res.status(404).json({
-            status: res.statusCode,
-            message: 'ProductId not Found'
-        });
-    }
-    else if(storageStatus == 403){
-        res.status(403).json({
-            status: res.statusCode,
-            message: 'Not Enough Products to reduce'
-        });
-    }
-    else if (storageStatus != 200) {
-        res.status(501).json({
-            status: res.statusCode,
-            message: 'The server does not support the functionality required to fulfill the request'
-        })
-    }
-    //Not working atm
-    //sendEmail(email)
-
-    res.json({
-        message: 'Shippment was succesfully sent',
-        status: res.statusCode,
-        shippingStatus: 'Shippment was sent to ' + email,
-        quantity: quantity,
-        id: id
-     });
-})
+    let authHeader = req.headers['authorization'];
+    apiFunction(id, quantity, email, authHeader, res);
+});
 
 // Storage Caller function
 async function storageCaller(id, quantity, authHeader){
     const options = {
-        headers: {'Content-Type': 'application/json',  'Authorization': `Bearer ${authHeader}`}
+        headers: {'Content-Type': 'application/json',  'Authorization': authHeader}
     }
     //Add correct API here 
     //TEST
-    let res = await axios.get('https://fakestoreapi.com/products/1')
+    let res = await axios.get('https://fakestoreapi.com/products/1');
     //INVENTORY API
     // Borde vara post men inventeroy apin kan patchas direkt ¯\_(ツ)_/¯
     /*let res = await axios.patch(`${inventoryService}`+id, {
@@ -101,9 +57,7 @@ async function storageCaller(id, quantity, authHeader){
         pris: '0',
         antal: quantity
     }, options); */
-    console.log('storageCaller check');
     return res.status;
-    
 }    
 
 //Send Email function
@@ -112,7 +66,6 @@ async function sendEmail(email, authHeader){
     const options = {
         headers: {'Content-Type': 'application/json',  'Authorization': authHeader /*`Bearer ${token}` */}
     }
-    //console.log(options);
     let res = await axios.post(`${emailService}`, {
         to: email, 
         subject: "Test mail Newest", 
@@ -121,64 +74,59 @@ async function sendEmail(email, authHeader){
 
     return res.status;
 }
-// JSON Body Endpoint
-router.post('/reduce/product', async(req,res) =>{
-    id = req.body.id;
-    quantity = req.body.quantity;
-    email = req.body.email;
-    product = req.body.productName
-    let authHeader = req.headers['authorization'];
 
+async function apiFunction(id, quantity, email, authHeader, res) {
     const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.status(401).send({
-        auth: false,
-        message: 'No token provided.'});
-    jwt.verify(token, config.secret, (err, user) => {
-    if (err) return res.status(500).send({ 
-        auth: false, 
-        message: 'Failed to authenticate token.' });
-  }); 
+    if (!token) {
+        return res.status(401).send({
+            auth: false,
+            message: 'No token provided.'});
+    }
+    try {
+        jwt.verify(token,config.secret);
+        
+    } catch (error) {
+        return res.status(500).send({ 
+            auth: false, 
+            message: 'Failed to authenticate token.' });   
+    }
 
-    if(id == null || quantity == null || product == null) {
-        res.status(400).json({
+    if(id == null || quantity == null || email == null) {
+        return res.status(400).json({
             status: res.statusCode,
             message: 'Id, Quantity or Product Name is missing'
         });
     }
     else if(!(quantity > 0)) {
        
-        res.status(400).json({
+        return res.status(400).json({
             status: res.statusCode,
             message: 'Quantity must be a number over 0'
         });
     }
     let storageStatus = await storageCaller(id, quantity, authHeader);
 
-    console.log(storageStatus + " 2nd check");
-
     if(storageStatus == 404){
-        res.status(404).json({
+        return res.status(404).json({
             status: res.statusCode,
             message: 'ProductId not Found'
         });
     }
     else if(storageStatus == 403){
-        res.status(403).json({
+        return res.status(403).json({
             status: res.statusCode,
             message: 'Not Enough Products to reduce'
         });
     }
     else if (storageStatus != 200) {
-        res.status(501).json({
+        return res.status(501).json({
             status: res.statusCode,
             message: 'The server does not support the functionality required to fulfill the request'
         })
     }
-    // Returns status 200 but mail is not sent --> email api
     let emailStatus = await sendEmail(email, authHeader);
     let emailSent;
     let message;
-    console.log(emailStatus + ' emailStatus');
 
     if (emailStatus != 200) {
         emailSent = false;
@@ -189,19 +137,24 @@ router.post('/reduce/product', async(req,res) =>{
         message = 'Package was succesfully shipped and email confirmation was sent to ' + email;
     }
 
-    console.log("Last check if everyrthing went well");
     res.json({
        message: message,
        status: res.statusCode,
        shippingStatus: 'Shippment was sent to ' + email,
        emailSent: emailSent,
        quantity: quantity,
-       id: id,
-       product: product
+       id: id
     });
+}
+// JSON Body Endpoint
+router.post('/reduce/product', async(req,res) =>{
+    id = req.body.id;
+    quantity = req.body.quantity;
+    email = req.body.email;
+    let authHeader = req.headers['authorization'];
+
+    apiFunction(id, quantity, email, authHeader, res);
 });
-
-
 
 router.get('/', (req,res) =>{
     res.json({
